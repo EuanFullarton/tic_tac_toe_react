b80b00106d0fe9740db1cceddf45cf11c00ce83e
Euan Fullarton <euanfullarton7@gmail.com>
2017-06-07 11:37:37 +0100

    button innerText changes on click

--- client/src/components/ButtonSelector.jsx
+++ client/src/components/ButtonSelector.jsx
@@ -3,27 +3,37 @@ import React from 'react';
 class ButtonSelector extends React.Component {
   constructor(props){
     super(props)
-    // this.handleChange = this.handleChange.bind(this);
+    this.buttonClicked = this.buttonClicked.bind(this);
   }
 
   render(){
     return (
       <div id='grid'>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
-      <button onClick={this.buttonClicked}>Test button</button>
+      <button value='Test' id='1' onClick={this.buttonClicked}>Test button</button>
+      <button id='2' onClick={this.buttonClicked}>Test button</button>
+      <button id='3' onClick={this.buttonClicked}>Test button</button>
+      <button id='4' onClick={this.buttonClicked}>Test button</button>
+      <button id='5' onClick={this.buttonClicked}>Test button</button>
+      <button id='6' onClick={this.buttonClicked}>Test button</button>
+      <button id='7' onClick={this.buttonClicked}>Test button</button>
+      <button id='8' onClick={this.buttonClicked}>Test button</button>
+      <button id='9' onClick={this.buttonClicked}>Test button</button>
       </div>
       )
   }
 
-  buttonClicked(){
-    console.log('button has been clicked');
+  buttonClicked(event){
+    const buttonClickedValue = event.target.value;
+    const buttonClickedId = event.target.id;
+
+    console.log('value: ', buttonClickedValue);
+    console.log('id: ', buttonClickedId);
+
+    const buttonToChange = document.getElementById(buttonClickedId);
+    buttonToChange.innerText = 'Hello';
+
+    console.log('button has been clicked. this = ', this);
+
   }
 
 }
